e4a39d53788bd5114a1c69adbece20666328456f
Haitham El Abdioui <152732703+elabdioui@users.noreply.github.com>
2025-09-04 13:54:23 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,14 +1,14 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_YxV2U3UDi', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '50bvqgnbbo6894u7abu7ta07le', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        client_secret: 'hpgqarui1v3sil2cqk5hmsmbhga13vrda6spdjje6veqk9f9f9r' ,
+        userPoolClientId: '3c9rn9nvkpmvml1cepnl94msqq', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
 
